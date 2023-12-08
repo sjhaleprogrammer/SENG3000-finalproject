042a9147565f22a532964db93054c5ff44c5f4d9
Samuel Hale <samworlds1231337@gmaiil.com>
2023-12-07 22:54:11 -0500

    work in progress

--- webui/src/webui.nim
+++ webui/src/webui.nim
@@ -4,7 +4,7 @@ when isMainModule:
   
 
   serve "127.0.0.1", 5001:  
-  
+    staticDir "views"
     staticDir "css"
     staticDir "fonts"
     staticDir "images"
@@ -12,3 +12,12 @@ when isMainModule:
 
     get "/":
       return FileResponse("index.html")
+
+    get "/home":
+      return FileResponse("views/home.html")
+
+    get "/login":
+      return FileResponse("views/login.html")
+
+    #get "/view2":
+      #return FileResponse("views/view2.html")
